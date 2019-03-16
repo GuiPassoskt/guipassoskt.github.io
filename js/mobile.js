1f4414eba8020a1e8fc134e00d618733e1b7454a
GuiPassoskt <guipassoskt@gmail.com>
2019-03-16 13:23:22 -0300

    Atualização de Banners, Condição para cor da logo no mobile

---
+++ js/mobile.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $('#logo').attr('src','img/42.png');
+    }
+}
